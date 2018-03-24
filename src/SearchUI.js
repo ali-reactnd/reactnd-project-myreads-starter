6b60e100fc04b51fee495c64fb53ef2a64db01ca
Ali Madooei <amadooei@sfu.ca>
2018-03-23 18:45:25 -0700

    Added a query string to state in SearchUI and it gets updated as user enters text in the search box

--- src/SearchUI.js
+++ src/SearchUI.js
@@ -6,7 +6,13 @@ import {BookUI} from "./BookUI";
 class SearchUI extends Component {
 
     state ={
-        books: []
+        books: [],
+        query: ""
+    }
+
+    updateQuery = (query) => {
+        this.setState({query});
+        console.log(this.state.query);
     }
 
     render() {
@@ -23,7 +29,9 @@ class SearchUI extends Component {
                         However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                         you don't find a specific author or title. Every search is limited by search terms.
                     */}
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input type="text" placeholder="Search by title or author"
+                            onChange={event => this.updateQuery(event.target.value)}
+                        />
 
                     </div>
                 </div>
