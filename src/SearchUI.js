07894646634f072e9e6d939dfa23b0045d60d695
Ali Madooei <amadooei@sfu.ca>
2018-03-23 19:14:00 -0700

    Added DebounceInput to SearchUI and further refactored its render method

--- src/SearchUI.js
+++ src/SearchUI.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {BookUI} from "./BookUI";
+import DebounceInput from 'react-debounce-input';
 
 class SearchUI extends Component {
 
@@ -18,34 +19,26 @@ class SearchUI extends Component {
     render() {
         return (
             <div className="search-books">
-                <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.props.flipSearchBoolean()}>Close</a>
-                    <div className="search-books-input-wrapper">
-                    {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                        <input type="text" placeholder="Search by title or author"
-                            onChange={event => this.updateQuery(event.target.value)}
-                        />
-
-                    </div>
-                </div>
-                {this.Shelf()}
+                {this.searchBar()}
+                {this.searchResultBookShelf()}
             </div>
         );
     }
 
-    Shelf = () => (
+    searchBar = () => (
+        <div className="search-books-bar">
+            <a className="close-search" onClick={() => this.props.flipSearchBoolean()}>Close</a>
+            <div className="search-books-input-wrapper">
+                <DebounceInput minLength={1} debounceTimeout={800}
+                    onChange={event => this.updateQuery(event.target.value)} />
+            </div>
+        </div>
+    )
+
+    searchResultBookShelf = () => (
         <div className="search-books-results">
             <ol className="books-grid">
-                {
-                    this.state.books.map( book => <li key={book.id}> <BookUI book={book} /> </li> )
-                }
+                { this.state.books.map( book => <li key={book.id}> <BookUI book={book} /> </li> ) }
             </ol>
         </div>
     );
